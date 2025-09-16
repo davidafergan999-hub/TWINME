7d28cb4fea7f64c44c65916ab4ea3771e6f211d5
davidafergan999-hub <davidafergan999@gmail.com>
2025-09-16 16:49:12 +0300

    APP

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{padding: 20}}>
+      <h1>שלום 👋</h1>
+      <p>האתר עלה! נתחיל להוסיף פיצ׳רים אחרי שהדיפלוי יעבור.</p>
+    </div>
+  );
+}
